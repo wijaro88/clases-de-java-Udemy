0eed17f1f17e56af697c793ed2f371ec43cf6c51
Wilson Rocha <wijaro88@gmail.com>
2024-02-19 13:28:16 -0500

    av1

--- Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -1,50 +1,30 @@
 
-// import java.util.Scanner;
-
-//mi clase en Java
-
-// public class HolaMundo {
-
-//     public static void main(String args[]) {
-//         String nombre;
-//         int libroId;
-//         double precio;
-//         boolean envio;
-
-//         var consola = new Scanner(System.in);
-        
-//         System.out.println("Proporcione el nombre:");
-//         nombre = consola.nextLine();
-//         System.out.println("Proporciona el ID");
-//         libroId = Integer.parseInt(consola.nextLine());
-//         System.out.println("Proporciona el precio:");
-//         precio= Double.parseDouble(consola.nextLine());
-        
-//         System.out.println("envio gratuito:(true or false)");
-//         envio=Boolean.valueOf(consola.nextLine());
-
-//         System.out.println(nombre+" #"+libroId);
-//         System.out.println("Precio: $" +precio);
-//         System.out.println("Envio Gratuito: " + envio);        
-//     }
-// }
-
 import java.util.Scanner;
 
+// mi clase en Java
+
 public class HolaMundo {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Proporciona el nombre:");
-        String nombre = scanner.nextLine();
-        System.out.println("Proporciona el id:");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.println("Proporciona el precio:");
-        double precio = Double.parseDouble(scanner.nextLine());
-        System.out.println("Proporciona el envio gratuito:");
-        boolean envioGratuito = Boolean.parseBoolean(scanner.nextLine());
- 
-        System.out.println(nombre + " #" + id);
-        System.out.println("Precio: $" + precio);
-        System.out.println("Envio Gratuito : " + envioGratuito);
+
+    public static void main(String args[]) {
+        int a = 3, b = 2;
+        var resultado = a + b;
+        System.out.println("resultado = " + resultado);
+
+        resultado = a - b;
+        System.out.println("resultado resta = " + resultado);
+
+        resultado = a * b;
+        System.out.println("resultado multiplicacion = " + resultado);
+
+        var resultado2 = 3.0 / b;
+        System.out.println("resultado = " + resultado2);
+
+        resultado = a % b;
+        System.out.println("resultado modulo = " + resultado);
+
+        if (b % 2 == 0)
+            System.out.println("numero par");
+        else
+            System.out.println("numero impar");
     }
 }
