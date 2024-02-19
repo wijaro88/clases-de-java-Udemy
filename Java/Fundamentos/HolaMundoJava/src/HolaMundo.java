d5d7457db307475d4737ee5811c337234f6415bc
Wilson Rocha <wijaro88@gmail.com>
2024-02-19 15:37:20 -0500

    av28

--- Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -6,22 +6,6 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var a = 3;
-        var b = 2;
-        //operador de igualdad ==
-        var c=(a==b);
-
-        System.out.println("c= " + c);
-
-        var d= a !=b;
-
-        System.out.println("d= " + d);
-
-        var cadena= "Hola";
-        var cadena2 = "Hola";
-        var e= cadena ==cadena2;//cmpara referencias de objetos
-        System.out.println(e);
-        var f=cadena.equals(cadena2);//comparamos contenido de cadenas
-        System.out.println(f);
+     
     }
 }
