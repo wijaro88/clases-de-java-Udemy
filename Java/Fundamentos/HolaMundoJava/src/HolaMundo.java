b2b45dcb7a080c0d237e731da4936ac4c07f97a1
Wilson Rocha <wijaro88@gmail.com>
2024-02-19 15:52:36 -0500

    av29-operadores condicionales

--- Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -6,6 +6,15 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-     
+     var a = -1;
+     var valorMinimo = 0;
+     var valorMaximo=10;
+
+     var resultado = a >= 0 && a<=10; //&& And ||or
+     if (resultado){
+        System.out.println("Dentro de Rango");
+     }else{
+        System.out.println("Fuera de Rango");
+     }
     }
 }
