4d321a8992ad22e24ef282f9320690dea390f820
Wilson Rocha <wijaro88@gmail.com>
2024-02-28 12:48:31 -0500

    avance 31 Precedencia de operadores

--- Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -6,5 +6,14 @@
 public class HolaMundo {
 
     public static void main(String args[]) {    
+        var x= 5;
+        var y=10;
+        var z= ++x + y--;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        var resultado=4+5*6/3;
+        System.out.println(resultado);
     }
 }
