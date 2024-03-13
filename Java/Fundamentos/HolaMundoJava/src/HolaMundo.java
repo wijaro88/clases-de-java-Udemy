49a4e176c50da2206ed8847160499f2444c3e9b7
Wilson Rocha <wijaro88@gmail.com>
2024-03-13 12:13:02 -0500

    avance 32 ejercicio case

--- Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
+++ Java/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -1,27 +1,28 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 // mi clase en Java
 
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var mes = 12;
-        var estacion = "Estacion desconocida";
-
-        switch(mes){
-            case 1: case 2: case 12:
-            estacion="Invierno";
-            break;
-            case 3: case 4: case 5:
-            estacion="Primavera";
-            break;
-            case 6: case 7: case 8:
-            estacion="Verano";
-            break;
-            case 9: case 10: case 11:
-            estacion="Otonio";
-            break;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporcionar la nota entre  0 a 10:");
+        int nota = Integer.parseInt(scanner.nextLine());
+        
+        if (nota>=9 && nota<=10 ){
+            System.out.println("A");
+        }else if(nota >=8 && nota < 9){
+            System.out.println("B");
+        }else if (nota >=7 && nota <8){
+            System.out.println("C");
+        }else if(nota>=6 && nota<7){
+            System.out.println("D");
+        }else if(nota>=0 && nota <6){
+            System.out.println("F");
+        }else {
+            System.out.println("valor desconocido");
         }
-        System.out.println("Estacion: " +estacion);
-    }
+        }
+         
+            
 }
