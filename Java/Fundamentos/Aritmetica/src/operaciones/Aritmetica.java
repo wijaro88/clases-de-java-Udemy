ac5089487e0e42370b769ec7627d164997e3e2ba
Wilson Rocha <wijaro88@gmail.com>
2024-03-14 18:14:01 -0500

    avance clases y objetos

--- Java/Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
+++ Java/Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
@@ -1,15 +1,20 @@
 package operaciones;
 
-
 public class Aritmetica {
+
     //Atributos de la Clase
     int a;
     int b;
-    
+
     //Metodo modificador de acceso
-    public void sumar(){
+    public void sumar() {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
-    
+
+    public int sumarConRetorno() {
+//        int resultado = a + b;
+//        return resultado;
+        return a + b;
+    }
 }
