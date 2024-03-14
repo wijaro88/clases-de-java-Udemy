f6ddd453979c87ae67dfadf5d2e4e6e072ae32d5
Wilson Rocha <wijaro88@gmail.com>
2024-03-13 21:38:22 -0500

    avance 40 clases em java

---
+++ Java/Fundamentos/Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,15 @@
+package operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la Clase
+    int a;
+    int b;
+    
+    //Metodo modificador de acceso
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
---
+++ Java/Fundamentos/HolaMundoJava/src/clases/Persona.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Persona {
+    //Atributos de la clase
+    String nombre;
+    String apellido;
+    
+    public void desplegarInformacion(){
+        System.out.println("Nombre: " +nombre);
+        System.out.println("Apellido:" +apellido);
+    }
+}
